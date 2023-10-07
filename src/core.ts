36535c4577176d4606b21ac831febb42bc605f63
mulf <mhmulford0@gmail.com>
2023-10-07 15:53:41 -0500

    add prettier

--- src/core.ts
+++ src/core.ts
@@ -6,33 +6,26 @@ import Irys from "@irys/sdk";
 const FILE_SIZE_LIMIT = 5_000_000;
 
 const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: function (_req, file, callback) {
-    callback(null, Date.now().toString() + file.originalname);
-  },
+    destination: "uploads/",
+    filename: function (_req, file, callback) {
+        callback(null, Date.now().toString() + file.originalname);
+    },
 });
 
 const upload = multer({
-  storage: storage,
-  limits: { fileSize: FILE_SIZE_LIMIT },
-  fileFilter(_req, file, cb) {
-    const validMimeTypes = [
-      "image/jpeg",
-      "image/png",
-      "image/gif",
-      "video/mp4",
-    ];
+    storage: storage,
+    limits: { fileSize: FILE_SIZE_LIMIT },
+    fileFilter(_req, file, cb) {
+        const validMimeTypes = ["image/jpeg", "image/png", "image/gif", "video/mp4"];
 
-    if (!validMimeTypes.includes(file.mimetype)) {
-      cb(
-        new Error(
-          "Invalid file type. Only jpg, png, gif, and mp4 image files are allowed."
-        )
-      );
-    }
+        if (!validMimeTypes.includes(file.mimetype)) {
+            cb(
+                new Error("Invalid file type. Only jpg, png, gif, and mp4 image files are allowed.")
+            );
+        }
 
-    cb(null, true);
-  },
+        cb(null, true);
+    },
 });
 
 export const uploadPropMedia = upload.single("prop-media");
@@ -40,15 +33,15 @@ export const uploadPropMedia = upload.single("prop-media");
 // Arweave/Bundlr
 const { PRIVATE_KEY, RPC_URL } = process.env;
 if (!PRIVATE_KEY || !RPC_URL) {
-  throw new Error("PRIVATE_KEY and RPC_URL env var required");
+    throw new Error("PRIVATE_KEY and RPC_URL env var required");
 }
 
 export async function getIrys() {
-  const irys = new Irys({
-    url: "https://devnet.irys.xyz",
-    token: "ethereum",
-    key: PRIVATE_KEY,
-    config: { providerUrl: RPC_URL },
-  });
-  return irys;
+    const irys = new Irys({
+        url: "https://devnet.irys.xyz",
+        token: "ethereum",
+        key: PRIVATE_KEY,
+        config: { providerUrl: RPC_URL },
+    });
+    return irys;
 }
--- src/server.ts
+++ src/server.ts
@@ -3,26 +3,30 @@ import express from "express";
 import { uploadPropMedia } from "./core.js";
 
 export function createServer() {
-  const app = express();
-  app.use(express.static(new URL("./public", import.meta.url).pathname));
+    const app = express();
+    app.use(express.static(new URL("./public", import.meta.url).pathname));
 
-  app.get("/", (_, res) => {
-    res.json({ message: "running" });
-  });
-
-  app.post("/upload", (req, res) => {
-    // TODO: check prop admin
-
-    uploadPropMedia(req, res, (err) => {
-      if (err) {
-        return res.json(err);
-      }
-
-      console.log(req.file?.filename);
-
-      res.json({ message: "file uploaded" });
+    app.get("/", (_, res) => {
+        res.json({ message: "running" });
     });
-  });
 
-  return app;
+    app.post("/upload", (req, res) => {
+        // TODO: check prop admin
+
+        uploadPropMedia(req, res, (err) => {
+            if (!req.file) {
+                return res.json({ error: "No file detected" });
+            }
+
+            if (err) {
+                return res.json(err);
+            }
+
+            console.log(`${req.file.destination}${req.file.filename}`);
+
+            res.json({ message: "file uploaded" });
+        });
+    });
+
+    return app;
 }
