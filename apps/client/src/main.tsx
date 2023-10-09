a7651e444279f5806fde77fb27c93fb5a5ddd9af
mulf <mhmulford0@gmail.com>
2023-10-09 17:02:23 -0500

    add rainbow kit

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./polyfills";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+if (!process.env.VITE_ALCHEMY_ID) {
+  throw new Error("alchemy api key required");
+}
+
+const { chains, publicClient } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: process.env.VITE_ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Nounish Media",
+  projectId: "dd78716fb7c73961f4f3fede8139b615",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
